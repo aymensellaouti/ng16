89cd227bb464a0b5b75c82f8735f9c9011df6b8d
aymen2782 <aymen.sellaouti@gmail.com>
2023-08-03 16:40:59 +0100

    Auto-generated commit [ci skip]

---
+++ 357.51b564a418f3af84.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkng16Standalone=self.webpackChunkng16Standalone||[]).push([[357],{3357:(h,o,n)=>{n.r(o),n.d(o,{CvRoutes:()=>d});const d=[{path:"",loadComponent:()=>n.e(374).then(n.bind(n,5374)).then(t=>t.CvComponent)},{path:"add",loadComponent:()=>n.e(798).then(n.bind(n,9798)).then(t=>t.AddComponent)}]}}]);
